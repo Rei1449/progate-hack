bae4ead810d2b2684a560597034df1d38f924c7f
z-fujimori <fuji.tai.29@i.softbank.jp>
2024-08-25 02:54:21 +0900

    QiitaAPI テスト

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,28 @@
-use actix_web::{get, App, HttpServer, Responder};
-
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+// use actix_web::{actix, client};
+use reqwest::Client;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[get("/")]
-async fn index() -> impl Responder {
-    "Hello world!"
+async fn index()  -> Result<impl Responder> {
+// async fn index()  -> impl Responder {
+    let client = Client::new(); // 1
+    let url = "https://qiita.com/api/v2/items/0e2a5a3d047e6b08c811";
+    let response = client
+        .get(url)
+        .header("Bearer", "トークン")
+        .send()
+        .await // 2
+        .expect("Failed to send request");
+    // let body = response.text().await; // 3
+    let body = response
+        .text()
+        .await
+        .expect("Failed to read response text"); // ここでResultをアンラップ
+    println!("{:#?}", body);
+    Ok(body)
 }
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
